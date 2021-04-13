f3034cba020dda0ef4fe1ec10bdc34e32874d01c
Derek Price <derekp098@gmail.com>
2021-04-12 19:20:16 -0700

    added things

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
 import './App.css';
+import DropDown from './components/DropDown';
 
 function App() {
+
+  const [dropDownList, setDropDownList] = useState(initilizeDropDownListTemp())
+
+  function initilizeDropDownListTemp() {
+    var list = [];
+
+    for(var i = 0; i < 10; i++) {
+      var obj = {
+        title: 'obj' + i,
+        index: i,
+        selected: false
+      };
+
+      list.push(obj);
+    }
+
+    return list;
+  }
+  
   return (
     <div className="App">
-
+      <DropDown dropDownList={dropDownList} title='DropDown List' setDropdownList={(list) => {setDropDownList(list)}} />
     </div>
   );
 }
